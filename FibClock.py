31d4184125530730cbec236b4705a4e861f9cdec
dvoets <daan_voets@hotmail>
2016-01-09 08:14:26 -0800

    improved clock, returns an RGB string for the 5 RGB leds

--- FibClock.py
+++ FibClock.py
@@ -4,13 +4,15 @@ from fSequence import fSeq
 import random
 
 class klok:
-    def __init__(self):
+    def __init__(self, fadeTime):
         # Initialize the time
         self.minute = datetime.now().minute / 5
         self.hour = datetime.now().hour
         if self.hour > 12:
             self.hour = self.hour - 12
         self.fHour, self.fMinute=self.fTime()
+        self.fadeTime = fadeTime
+        self.timeColor = self.fTimeColor()
 
     def new_time(self):
         # Check if the difference between the time is 5 minutes
@@ -22,22 +24,45 @@ class klok:
             return True
         else:
             return False
+
     def fTime(self):
         posFibDecom=fSeq(5)
         return random.choice(posFibDecom.fDecom[self.hour]), random.choice(posFibDecom.fDecom[self.minute])
 
+    def fTimeColor(self):
+        timeColor = ['W','W', 'W', 'W', 'W']
+        for i in range(len(self.fHour)):
+            if self.fMinute[i] and self.fHour[i]:
+                timeColor[i]='B'
+            elif self.fMinute[i]:
+                timeColor[i]='G'
+            elif self.fHour[i]:
+                timeColor[i]='R'
+        return timeColor
+
+
+
 if __name__ == '__main__':
-    test = klok()
+    test = klok(0)
+    print 'Fade Time: ', test.fadeTime
     print 'werkt'
+    print test.timeColor
+    print '------------------'
+    print '|2 2 0 4 4 4 4 4 |'
+    print '|2 2 1 4 4 4 4 4 |'
+    print '|3 3 3 4 4 4 4 4 |'
+    print '|3 3 3 4 4 4 4 4 |'
+    print '|3 3 3 4 4 4 4 4 |'
+    print '------------------'
     print 'Readable Time\t\t', datetime.now().hour, datetime.now().minute
-    print 'Non Readable Time\t\t', test.hour, test.minute
+    print 'Non Readable Time\t', test.hour, test.minute
     print 'Fib Hours\t', test.fHour
     print 'Fib Minutes\t', test.fMinute
-    for i in range(1000):
-        sleep(10)
+    while True:
         if test.new_time():
             print 'Nieuwe tijd'
             print 'Readable Time\t\t', datetime.now().hour, datetime.now().minute
             print 'Non Readable Time\t\t', test.hour, test.minute
             print 'Fib Hours\t', test.fHour
             print 'Fib Minutes\t', test.fMinute
+            print test.timeColor
