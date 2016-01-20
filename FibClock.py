de36edb7f6d4f4c0e9fd83fe382ca523457ac821
dvoets <daan_voets@hotmail.com>
2016-01-20 19:17:05 +0000

    alpha release with neopixel

--- FibClock.py
+++ FibClock.py
@@ -4,10 +4,11 @@ from fSequence import fSeq
 import random
 
 class klok:
-    def __init__(self, fadeTime):
+    def __init__(self, fadeTime, lightIntensity = 50):
         # Constant for fading
         # Initialize the time
         self.fadeTime = fadeTime
+        self.lightIntensity = lightIntensity
         now = datetime.now() + timedelta(seconds=self.fadeTime)
         self.minute = now.minute / 5
         self.hour = now.hour
@@ -25,6 +26,13 @@ class klok:
         # Fib time converted into colors (G=Hours, R=Minutes, B=Hours and Minutes)
         self.timeColor = self.fTimeColor()
         self.timeRGB = self.fTimeRGB() 
+        self.prev_timeRGB = self.fTimeRGB()
+        print 'RED \t= Hours'
+        print 'GREEN \t= Minute'
+        print 'BLUE \t= Hours AND Minute'
+        print 'TIME NOW:', datetime.now()
+        print 'Colors Time Now: ', self.timeColor
+        print 'RGB Time Now: ', self.timeRGB
 
     def new_time(self):
         # Check if the difference between the time is 5 minutes
@@ -34,10 +42,18 @@ class klok:
             self.prev_hour = self.hour
             self.prev_minute = self.minute
             self.hour = now.hour
-            self.minute = now.minute / 5
+            self.minute = minute
+            print self.hour, self.minute
+            if self.hour > 12:
+                self.hour = self.hour - 12
             self.fHour, self.fMinute = self.fTime()
             self.timeColor = self.fTimeColor()
-            self.timeRGB = self.fTimeRGB() 
+            self.prev_timeRGB = self.timeRGB
+            self.timeRGB = self.fTimeRGB()
+            print 'NEW TIME', datetime.now()
+            print 'Colors New Time: ', self.timeColor
+            print 'RGB prev Time: ', self.prev_timeRGB
+            print 'RGB New Time: ', self.timeRGB
             return True
         else:
             return False
@@ -62,32 +78,22 @@ class klok:
         for i in range(5):
             #Blue
             if self.fMinute[i] and self.fHour[i]:
-                fRGB[i]= [0, 0, 255]
+                fRGB[i]= [0, 0, self.lightIntensity]
             #Green
             elif self.fMinute[i]:
-                fRGB[i]= [0, 255, 0]
+                fRGB[i]= [0, self.lightIntensity, 0]
             #Red
             elif self.fHour[i]:
-                fRGB[i]= [255, 0, 0]
+                fRGB[i]= [self.lightIntensity, 0, 0]
             #White
             else:
-                fRGB[i]= [255, 255, 255]
+                fRGB[i]= [self.lightIntensity/3, self.lightIntensity/3, self.lightIntensity/3]
         return fRGB
 
 
 
 if __name__ == '__main__':
-    test = klok(4)
-    print 'TIME NOW:', datetime.now()
-    print test.timeColor
-    print "Previous Time: ", test.prev_hour, ':', test.prev_minute
-    print "New Time: ", test.hour, ':', test.minute
-    print test.timeRGB
+    test = klok(120, 40)
     while True:
         if test.new_time():
             print 'NEW TIME'
-            print 'TIME NOW:', datetime.now()
-            print test.timeColor
-            print "Previous Time: ", test.prev_hour, ':', test.prev_minute
-            print "New Time: ", test.hour, ':', test.minute
-            print test.timeRGB
--- main.py
+++ main.py
@@ -1,4 +1,41 @@
+from FibClock import *
+import time
+from neopixel import *
+
+# LED strip configuration:
+LED_COUNT      = 5       # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
 
 if __name__ == "__main__":
-        main()
+    print "werk ik wel?"
+    fade = 10
+    test = klok(fade, 30)
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, Color(test.timeRGB[i][0], test.timeRGB[i][1], test.timeRGB[i][2]))
+    strip.show()
+    while True:
+        if test.new_time():
+            for j in range(int(fade/0.1)):
+                #print "FADING "
+                if j != 0:
+                    #k = float(j)
+                    #print 'Prev', test.prev_timeRGB, 'Now', test.timeRGB, 'j', j, 'fade', fade
+                    time.sleep(0.1)
+                    for i in range(strip.numPixels()):
+                            prop = j/(fade / 0.1)
+                            #print prop
+                            RED = int(test.prev_timeRGB[i][0] - (test.prev_timeRGB[i][0] - test.timeRGB[i][0])*prop)
+                            GREEN = int(test.prev_timeRGB[i][1] - (test.prev_timeRGB[i][1] - test.timeRGB[i][1])*prop)
+                            BLUE = int(test.prev_timeRGB[i][2] - (test.prev_timeRGB[i][2] - test.timeRGB[i][2])*prop)
+                            #print RED, GREEN , BLUE
+                            strip.setPixelColor(i, Color(RED, GREEN, BLUE))
+                    strip.show()
+
